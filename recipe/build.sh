0efc14877d54f0fc44775534338bbddb0ed3e0a9
ViridianMelody <mlove@anaconda.com>
2025-12-02 12:35:11 -0800

    Disable udev_kms

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-meson setup builddir --prefix="${PREFIX}" -Dglx=false -Dxquartz=false -Dsecure-rpc=false -Dxorg=false -Dudev=false
+meson setup builddir --prefix="${PREFIX}" -Dglx=false -Dxquartz=false -Dsecure-rpc=false -Dxorg=false -Dudev=false -Dudev_kms=false
 
 cd builddir
 
