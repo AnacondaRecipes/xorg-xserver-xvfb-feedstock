98fa4b9ecfa5cdbbb8048c263ba278ffe316281b
avetr-work <avetroumov@anaconda.com>
2026-09-03 10:50:57 +0300

    PKG-17647: rebuild xorg-xserver-xvfb 21.1.24 (#4)
    * PKG-17647: rebuild xorg-xserver-xvfb 21.1.24
    
    * Rebuild
    
    * Set exact pinning in host
    
    * Fix osx tests
    
    * Fix build.sh
    
    * CHeck for enviroonment paths
    
    * Remove debug info, check for specific evdev for osx
    
    * Check for osx fix
    
    * Enablke test for osx
    
    * Disable test for osx with comment
    
    * Fix . review findings

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-meson setup builddir --prefix="${PREFIX}" -Dglx=false -Dxquartz=false -Dsecure-rpc=false -Dxorg=false -Dudev=false -Dudev_kms=false
+meson setup builddir --prefix="${PREFIX}" -Dglx=false -Dxquartz=false -Dsecure-rpc=false -Dxorg=false -Dudev=false -Dudev_kms=false -Dxkb_dir="${PREFIX}/share/X11/xkb"
 
 cd builddir
 
